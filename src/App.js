7208d4a4f0c20640b8488c74fe7242a0eb685188
jobdk <johnybudnik+git@gmail.com>
2021-05-16 08:54:35 +0200

    fixed import

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <div className="App">
+            <p>hello</p>
+        </div>
+    );
 }
 
 export default App;
