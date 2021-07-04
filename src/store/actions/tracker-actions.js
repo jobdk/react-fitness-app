ab6e567af77d8567565f07b7965aa87961f20828
jobdk <johnybudnik+git@gmail.com>
2021-07-04 09:21:10 +0200

    asdasd

---
+++ src/store/actions/tracker-actions.js
@@ -0,0 +1,71 @@
+import axios from "../../axios-url";
+
+export const STORE_DAY = "STOREDAY";
+
+const storeDay = (day) => {
+  return {
+    type: STORE_DAY,
+    payload: day,
+  };
+};
+
+export const getProfiles = () => {
+  return (dispatch) => {
+    axios
+      .get("/fitness/profiles/", { withCredentials: true })
+      .then((response) => {
+        dispatch(storeDay(response.data));
+      })
+      .catch((error) => {
+        alert(error.response.data);
+      });
+  };
+};
+
+export const deleteProfile = (profileId) => {
+  return (dispatch) => {
+    axios
+      .delete("/fitness/profile/:" + profileId, { withCredentials: true })
+      .then((response) => {
+        if (
+          response.data.msg === "Deleting all days for this profile went wrong!"
+        ) {
+          alert(response.data.msg);
+        }
+
+        // console.log(response);
+        // console.log(response.data);
+      })
+      .catch((error) => {
+        alert(error.response.data);
+      });
+  };
+};
+
+export const postProfile = (profile) => {
+  return (dispatch) => {
+    axios
+      .post("/fitness/profile/", profile, { withCredentials: true })
+      .then((response) => {
+        console.log(response);
+        //dispatch(storeFoods(response.data));
+      })
+      .catch((error) => {
+        alert(error.response.data);
+      });
+  };
+};
+
+export const putProfile = (profile) => {
+  return (dispatch) => {
+    axios
+      .put("/fitness/profile/", profile, { withCredentials: true })
+      .then((response) => {
+        console.log(response);
+        //dispatch(storeProfiles(response.data));
+      })
+      .catch((error) => {
+        alert(error.response.data);
+      });
+  };
+};
